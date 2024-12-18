f6b7300af120bf8f8a0a51fd5de0e74eb89ec53c
Vaimbhav <vaimbhav05@gmail.com>
2024-12-18 22:26:15 +0530

    Final Changes

--- src/hooks/use-user.js
+++ src/hooks/use-user.js
@@ -1,29 +1,3 @@
-// import {useState, useEffect, useContext} from 'react';
-
-// import UserContext from '../context/user';
-// import {getUserByUserId} from '../services/firebase';
-
-// const useUser = () => {
-// 	const [activeUser, setActiveUser] = useState({});
-// 	const {user} = useContext(UserContext);
-
-// 	useEffect(() => {
-// 		async function getUserObjByUserId() {
-// 			// we need a function so that we can call (firebase service) that gets the user data based on the id
-// 			const [response] = await getUserByUserId(user.uid);
-// 			setActiveUser(response);
-// 		}
-
-// 		if (user?.uid) {
-// 			getUserObjByUserId();
-// 		}
-// 	}, [user]);
-
-// 	return {user: activeUser};
-// };
-
-// export default useUser;
-
 import {useState, useEffect, useContext} from 'react';
 import UserContext from '../context/user';
 import {getUserByUserId} from '../services/firebase';
